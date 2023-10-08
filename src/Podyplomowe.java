a9edb39a64c819f464b82beccaab537c39cec652
kotlinow <kotlinowski@vp.pl>
2023-10-08 16:15:37 +0200

    upgrade

--- src/Podyplomowe.java
+++ src/Podyplomowe.java
@@ -1,6 +1,6 @@
 public class Podyplomowe {
     public static void main(String[] args) {
         System.out.println("Hello podyplomowe");
-
+        System.out.println("Hello podyplomowe upgrade");
     }
 }
